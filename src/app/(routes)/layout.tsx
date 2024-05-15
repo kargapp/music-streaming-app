cd24a47e3829456b3c72e7d5d95c983488825fb3
kargapp <122487930+kargapp@users.noreply.github.com>
2024-05-14 17:42:30 -0700

    refactor: removed next.js boilerplate

---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "SpotiBye",
+	description: "Another music streaming app!",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main>
+			<div className="text-green-600">Hello World</div>
+		</main>
+	);
+}
